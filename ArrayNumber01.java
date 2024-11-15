bdae4e5d182c5c5b66c8ed14012dc92f706d9586
hamdirafi <hamdirafi310@gmail.com>
2024-11-15 09:47:21 +0700

    Add files via upload

---
+++ ArrayNumber01.java
@@ -0,0 +1,16 @@
+public class ArrayNumber01 {
+    public static void main(String[] args) {
+    int[] num = new int [4];
+
+    num[0] = 5;
+    num[1] = 12;
+    num[2] = 7;
+    num[3] = 10;
+
+    System.out.println(num[0]);
+    System.out.println(num[1]);
+    System.out.println(num[2]);
+    System.out.println(num[3]);
+    
+    }
+}
