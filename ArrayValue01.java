1180b19c95c2c8136871d87955a7cf66f7b120a7
hamdirafi <hamdirafi310@gmail.com>
2024-11-15 10:06:08 +0700

    Add files via upload

---
+++ ArrayValue01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayValue01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] finalScore = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the final score " + i + ": ");
+            finalScore[i] = sc.nextInt();
+        }
+
+        System.out.println("/nDisplaying the entered scores:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Final score" + i + " is " + finalScore[i]);
+        }
+        
+        sc.close();
+    }
+}
